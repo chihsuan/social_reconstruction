b442c4a31cb8b593070b49a1a9a1a2b787f44a34
Chi-Hsuan <chihsuan.huang.7@gmail.com>
2014-05-06 16:13:41 +0800

    move some in main to function and use Synchronizing Threads

--- movieAnalytics.py
+++ movieAnalytics.py
@@ -27,7 +27,6 @@ FLANN_INDEX_LSH    = 6
 #which directory the result to output
 OUTPUT_PATH = 'output/'
 
-
 # mutithread for increase speed
 class Pthread (threading.Thread):
     
@@ -44,10 +43,10 @@ class Pthread (threading.Thread):
     def run(self):
         global faceLists
         print self.name + ' start'
-        
+        threadLock.acquire() 
         #merge faceLists
         mergeByFaceMatch(self.keyword, self.startPoint, self.finishPoint, self.minCount, self.mergeCount) 
-
+        threadLock.release()
 
 
 # merge different face but maybe same character
@@ -65,43 +64,6 @@ def mergeByPosition(face1, face2):
             face2.character = face1.character
             return True
     return False
-
-
-
-#merge by Imaging Technology
-def mergeByFaceMatch(keyword, startPoint, endPoint, minCount, mergeCount):
-  
-    remove = []
-    key = []
-    for character in faceLists[keyword]:
-        key.append(character)
-    
-    endPoint = len(faceLists[keyword]) 
-    try:
-        matchCount = 0
-        for i in range(startPoint, endPoint):
-            for j in range(startPoint, endPoint):
-                if i == j:
-                    continue
-                for face1 in faceLists[keyword][key[i]]:
-                    for face2 in faceLists[keyword][key[j]]:
-                        if faceCV.match(minCount, face1, face2) and faceCV.match(minCount, face2, face1):
-                            print 'merge!'
-                            print keyword
-                            matchCount += 1  
-                            if matchCount == mergeCount:
-                                faceLists[keyword][key[i]] += faceLists[keyword][key[j]]
-                                faceLists[keyword][key[j]] = []
-                                remove.append(j)
-                                break
-                    if matchCount == mergeCount:
-                        matchCount = 0
-                        break
-    except:
-        tp, val, tb = sys.exc_info()
-        print  sys.stderr, str(tp), str(val)
-    for j in remove:
-        del faceLists[keyword][key[j]]
 
 
 def faceClassification(faceList, pivotKeyword, minute, exclude):
@@ -181,143 +143,224 @@ def findCharacter(faceList, nameKeyword, sec):
     return nameList  
 
 
+def findRelationship():
+   
+    relationList = {}
+    wordClassified.append(leadingKeyword)
+    exclude = [leadingKeyword]
+    for keyword in keywordDic:
+            tmpList, tmpClassified = faceClassification(faceList, keyword, 3, exclude)
+            exclude.append(keyword)
+            relationList[keyword] = tmpClassified[:]
+            print keyword, relationList[keyword]
+    
+    return relationList
+
+#merge by Imaging Technology
+def mergeByFaceMatch(keyword, startPoint, endPoint, minCount, mergeCount):
+  
+    remove = []
+    key = []
+    for character in faceLists[keyword]:
+        key.append(character)
+    
+    endPoint = len(faceLists[keyword]) 
+    try:
+        matchCount = 0
+        for i in range(startPoint, endPoint):
+            for j in range(startPoint, endPoint):
+                if i == j:
+                    continue
+                for face1 in faceLists[keyword][key[i]]:
+                    for face2 in faceLists[keyword][key[j]]:
+                        if faceCV.match(minCount, face1, face2) and faceCV.match(minCount, face2, face1):
+                            print 'merge!'
+                            print keyword
+                            matchCount += 1  
+                            if matchCount == mergeCount:
+                                faceLists[keyword][key[i]] += faceLists[keyword][key[j]]
+                                faceLists[keyword][key[j]] = []
+                                remove.append(j)
+                                break
+                    if matchCount == mergeCount:
+                        matchCount = 0
+                        break
+    except:
+        tp, val, tb = sys.exc_info()
+        print  sys.stderr, str(tp), str(val)
+    for j in remove:
+        del faceLists[keyword][key[j]]
+
+
+
+def outputJson(characterList):
+   
+    #for record pair of character and node ID
+    characterNode = {}
+
+    #bulid nodes
+    nodes = []
+    i = 0
+    for character in characterList:
+        if character.keyword in characterNode:
+            continue
+        else:
+            nodes.append({'group':i, 'name': character.keyword, 'ID': i })
+            characterNode[character.keyword] = i
+            i += 1
+
+    #bulid edges
+    edges = []
+    for i in range(0, len(characterList), 2):
+        edges.append( {'source': characterNode[characterList[i].keyword], 'target':characterNode[characterList[i+1].keyword], 'value': 5, 'label': characterList[i+1].keyword } ) 
+        edges.append( {'source': characterNode[characterList[i+1].keyword], 'target':characterNode[characterList[i].keyword], 'value': 5, 'label': characterList[i].keyword } ) 
+
+    for source in relationList:
+        for target in relationList[source]:
+            edges.append( {'source': characterNode[source], 'target': characterNode[target], 'value': 5, 'label': target }  )
+            edges.append( {'source': characterNode[target], 'target': characterNode[source], 'value': 5, 'label': source }  )
+
+    #json dumps
+    print json.dumps({ 'nodes':nodes, 'links': edges})
+ 
+    #output json file
+    with open(OUTPUT_PATH + '/result/data.json', 'w') as outfile:
+        json.dump({'nodes':nodes, 'links': edges}, outfile, indent = 4)
+
+
+
+def outputResultImage(characterList, characterSort):
+
+    for word in characterSort:
+        count = -1
+        if '-' in word:
+            keyword = word.split('-')[1]
+        else:
+            keyword = word
+       
+        for character in characterSort[word]:
+            if count >= 1:
+                img = cv2.imread(OUTPUT_PATH + '/img/' + str(character.getFrame()) + '-' + '1.jpg' )
+                cv2.imwrite(OUTPUT_PATH + '/result/' + keyword + str(count) + '.jpg', img)
+            count += 1
+
+    for character in characterList:
+        point = character.getPoint()
+        img = character.getImg()
+        face = img
+        face = cv2.imread(OUTPUT_PATH + '/img/' + str(character.getFrame()) + '-' + '1.jpg' )
+        #face = img[point.y1:point.y2, point.x1:point.x2]
+        cv2.imwrite(OUTPUT_PATH + '/result/' + str(character.keyword) + '0.jpg', face)
+        cv2.imwrite(OUTPUT_PATH + '/character/' + str(character.keyword) + '.jpg', face)
+
+
 if __name__ == '__main__':
 
 
-    global faceLists 
-    global faceCV 
-    mergeList = {}
-
-    #initailize
+    #initailize data object
     movieData  = MovieData( OUTPUT_PATH + 'preprocessedData.txt')
-    
     #get data from preprocessing.py ouputfile
     faceList, keywords = movieData.getFaceAndKeyword(0, OUTPUT_PATH)
     print 'Face Number=', len(faceList)   
     
+    #initailize faceCV object
     faceCV = FaceCV(len(faceList))
     detector, matcher = faceCV.init_feature('sift')
     
 
+   
+    # see whether the face keyword and image are matched
     store = {}
     for face in faceList:
         store[face.getID()] = face 
         cv2.imwrite(OUTPUT_PATH + '/tmp/' + face.keyword + str(face.getID()) + '.jpg', face.getImg() )
-        
+     
+     
+    #step1 find face in near position
+    print 'Step1 merge faceList by position'
+    for i in range(0, len(faceList) ):
+        for j in range(i, len(faceList) ):
+            mergeByPosition(faceList[i], faceList[j])
+
+    
+    #step2 find the leadingRole's keyword
+    print 'step2 find the leadingRole\'s keyword'
     keywordDic = {}
     for face in faceList:
         if face.keyword not in keywordDic:
             keywordDic[face.keyword] = []
         keywordDic[face.keyword].append(face)
    
+    # get the leadingRole's keyword
     tmp = 0
     for key in keywordDic:
         if len(keywordDic[key]) > tmp:
             tmp = len(keywordDic[key])
             leadingKeyword = key
-    keywords = keywordDic.keys()
-
-
-    #keywords.sort( key = keywordDic.__getitem__)
-    print leadingKeyword 
-   
-    #step1 find face in near position
-    if len(sys.argv) > 3:
-        print 'Step1 merge faceList by position'
-        for i in range(0, len(faceList) ):
-            for j in range(i, len(faceList) ):
-                mergeByPosition(faceList[i], faceList[j])
-        outputCharacter(faceList)
-    else:    
-        readCharacter(faceList)
-
-    if len(sys.argv) > 1:
-        
-        #print 'Step2 merge faceList by position'
-        #step2 merge faceList -> faceLists by above result
-
-        #sort face by frame position
-        faceList.sort(key = lambda x: x.getFrame())
-        for face in faceList:
-            print face.getFrame(),
- 
-
-        #classfiy if a time interval only has keyword and leadingKeyword 
-        classifiedList, wordClassified =  faceClassification(faceList, leadingKeyword, 0.5, [])
-        
-        #add leadingRoleList to list
-        classifiedList[leadingKeyword] = keywordDic[leadingKeyword]
-        
-        
-        relationList = {}
-        wordClassified.append(leadingKeyword)
-        exclude = [leadingKeyword]
-
-        for keyword in keywordDic:
-                tmpList, tmpClassified = faceClassification(faceList, keyword, 3, exclude)
-                exclude.append(keyword)
-                relationList[keyword] = tmpClassified[:]
-                print keyword, relationList[keyword]
-
-        #add keyword that did not add before
-        for key in keywordDic:
-            if key not in wordClassified:
-                print key
-                classifiedList[key] = keywordDic[key]
-
-        #build a faceLists dictionary by keyword and characterID
-        faceLists = {}
-        for key in classifiedList:
-            for face in classifiedList[key]:
-                if face.keyword not in faceLists:
-                    faceLists[face.keyword] = {}
-                if face.character not in faceLists[face.keyword]:
-                    faceLists[face.keyword][face.character] = []
-                    faceLists[face.keyword][face.character].append(face)
-                else:
-                     faceLists[face.keyword][face.character].append(face)
-
-
-    if len(sys.argv) > 2: 
-
-        print 'Step3 merge faceList by match'
     
-        
-        #initailize mutithread
-        minCount = 5
-        mergeCount = 1
-        threadID = 0
+    print leadingKeyword 
 
-        
-        threads = []
-        for keyword in faceLists:
-            if keyword == leadingKeyword:
-                continue
-            thread = Pthread(threadID, 'Thread-'+ str(threadID), keyword, 0, len(faceLists[keyword].keys()), minCount, mergeCount)
-            threadID += 1
-            thread.start()
-            threads.append(thread)
-      
-        mergeByFaceMatch(leadingKeyword, 0, len(faceLists[leadingKeyword].keys()), 20, mergeCount)
+    print 'step3 start find the relation graph'
 
-        #wait all threads complete 
-        for thread in threads:
-            thread.join()
-         
+    #sort face by frame position
+    faceList.sort(key = lambda x: x.getFrame())
 
-        outputMergeResult(faceLists, 'faceLists.txt')
+    #classfiy if a time interval only has keyword and leadingKeyword (relation)
+    classifiedList, wordClassified =  faceClassification(faceList, leadingKeyword, 0.5, [])
+    
+    #add leadingRoleList to list
+    classifiedList[leadingKeyword] = keywordDic[leadingKeyword]
+    
+    #add keyword that did not add before
+    for key in keywordDic:
+        if key not in wordClassified:
+            print key
+            classifiedList[key] = keywordDic[key]
+    
+    #find other character and another character's relation
+    relationList = findRelationship()
 
-    readMergeResult(mergeList, store, 'faceLists.txt')
-    print len(mergeList)
+    #build a faceLists (graph) by keyword and characterID
+    faceLists = {}
+    for key in classifiedList:
+        for face in classifiedList[key]:
+            if face.keyword not in faceLists:
+                faceLists[face.keyword] = {}
+            if face.character not in faceLists[face.keyword]:
+                faceLists[face.keyword][face.character] = []
+                faceLists[face.keyword][face.character].append(face)
+            else:
+                 faceLists[face.keyword][face.character].append(face)
 
-    #step4 start anlaytic
-    print 'step4 start anlaytic'
 
+    print 'Step4 merge faceList by match'
+    #initailize mutithread
+    minCount = 5
+    mergeCount = 1
+    threadID = 0
 
+    
+    keywords = keywordDic.keys()
+    threadLock = threading.Lock()
+    threads = []
+    for keyword in faceLists:
+        if keyword == leadingKeyword:
+            continue
+        thread = Pthread(threadID, 'Thread-'+ str(threadID), keyword, 0, len(faceLists[keyword].keys()), minCount, mergeCount)
+        threadID += 1
+        thread.start()
+        threads.append(thread)
+  
+    mergeByFaceMatch(leadingKeyword, 0, len(faceLists[leadingKeyword].keys()), 15, mergeCount)
+
+    #wait all threads complete 
+    for thread in threads:
+        thread.join()
+
+    #step5 start anlaytic
+    print 'step5 start anlaytic find character'
     bipartitleGraph = faceLists
-
     characterList = []
-    print 'step5 find character'
     
     #find leading role
     sortList =  sorted(bipartitleGraph[leadingKeyword], key = lambda k: len(bipartitleGraph[leadingKeyword][k]), reverse = True) 
@@ -366,10 +409,10 @@ if __name__ == '__main__':
                 characterSort[word][1] = characterSort[word][move-1]
                 characterSort[word][move-1] = temp
             print move
-    detector, matcher = faceCV.init_feature('orb') 
 
-
+    
     #use leadingRole face match rate to decide character1 and character2 which is leadingRole
+    detector, matcher = faceCV.init_feature('orb') 
     for i in range(0, len(characterList), 2):
         print i
         matchCount1 = 0
@@ -393,58 +436,7 @@ if __name__ == '__main__':
             characterList[i].keyword = keywords[1]
             characterList[i+1] = leadingRole[0].copy()
 
-
-    #bulid nodes
-    nodes = []
-    characterNode = {}
-    
-    i = 0
-    for character in characterList:
-        if character.keyword in characterNode:
-            continue
-        else:
-            nodes.append({'group':i, 'name': character.keyword, 'ID': i })
-            characterNode[character.keyword] = i
-            i += 1
-
-    #bulid edges
-    edges = []
-    for i in range(0, len(characterList), 2):
-        edges.append( {'source': characterNode[characterList[i].keyword], 'target':characterNode[characterList[i+1].keyword], 'value': 5, 'label': characterList[i+1].keyword } ) 
-        edges.append( {'source': characterNode[characterList[i+1].keyword], 'target':characterNode[characterList[i].keyword], 'value': 5, 'label': characterList[i].keyword } ) 
-
-    for source in relationList:
-        for target in relationList[source]:
-            edges.append( {'source': characterNode[source], 'target': characterNode[target], 'value': 5, 'label': target }  )
-            edges.append( {'source': characterNode[target], 'target': characterNode[source], 'value': 5, 'label': source }  )
-
-
-    print json.dumps({ 'nodes':nodes, 'links': edges})
- 
-    with open(OUTPUT_PATH + '/result/data.json', 'w') as outfile:
-        json.dump({'nodes':nodes, 'links': edges}, outfile, indent = 4)
-
-    for word in characterSort:
-        count = -1
-        if '-' in word:
-            keyword = word.split('-')[1]
-        else:
-            keyword = word
-       
-        for character in characterSort[word]:
-            if count >= 1:
-                img = cv2.imread(OUTPUT_PATH + '/img/' + str(character.getFrame()) + '-' + '1.jpg' )
-                cv2.imwrite(OUTPUT_PATH + '/result/' + keyword + str(count) + '.jpg', img)
-            count += 1
-
-    for character in characterList:
-        point = character.getPoint()
-        img = character.getImg()
-        face = img
-        face = cv2.imread(OUTPUT_PATH + '/img/' + str(character.getFrame()) + '-' + '1.jpg' )
-        #face = img[point.y1:point.y2, point.x1:point.x2]
-        cv2.imwrite(OUTPUT_PATH + '/result/' + str(character.keyword) + '0.jpg', face)
-        cv2.imwrite(OUTPUT_PATH + '/character/' + str(character.keyword) + '.jpg', face)
-    
-       # cv2.imwrite(OUTPUT_PATH + '/result/' + str(character.keyword) + '-' + str(character.character) + '2.jpg', img)
-       
+    print 'output result...'
+    #output the final result
+    outputJson(characterList)
+    outputResultImage(characterList, characterSort)
