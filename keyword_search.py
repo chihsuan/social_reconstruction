aa1b4dd13ff029df8bee4b529b7ca493209930d0
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-07-30 20:20:38 +0800

    update keyword_search output

--- keyword_search.py
+++ keyword_search.py
@@ -5,7 +5,8 @@ Define Keyword: keyword is name or term relationship.
 ex: Jack, Dad
 
 Intput: -> 1 name_file 2. realationship_file 2. subtitle_file
-Output: keyword to time in subtitle: keyword_time_csv_file
+Output: keyword to time in subtitle and keyword_list 
+        -> 1.keyword_time_csv_file 2. keyword_list
 '''
 
 import sys
@@ -25,27 +26,32 @@ def keyword_search(name_file, relationship_file, subtitle_file):
     name_patterns = {}
     for name in name_list:
         name_patterns[name] = '\\b' + name.lower() + '\\b' 
-    
+
     relation_patterns = {}
     for relation in relation_list:
         relation_patterns[relation] = '\\b' + relation.lower() + '\\b' 
-    
+
     time_to_keyword = []
     subtitle_interval = []
     keyword_number = 0
+    keyword_list = []
     for line in subtitle:
         if line.strip():
             if len(subtitle_interval) == 1:
                 subtitle_time = line[:-2]
             for name in name_patterns:
                 if keyword_number < MAX_KEYWORDS_IN_ONE_INTERVAL and re.search(name_patterns[name], line.lower()):
-                   time_to_keyword.append([subtitle_time, name])
-                   keyword_number += 1
-
+                    time_to_keyword.append([subtitle_time, name])
+                    keyword_number += 1
+                    if name not in keyword_list:
+                       keyword_list.append(name)
+                        
             for relation in relation_patterns:
                 if keyword_number < MAX_KEYWORDS_IN_ONE_INTERVAL and re.search(relation_patterns[relation], line.lower()):
-                   time_to_keyword.append([subtitle_time, relation])
-                   keyword_number += 1
+                    time_to_keyword.append([subtitle_time, relation])
+                    keyword_number += 1
+                    if relation not in keyword_list:
+                       keyword_list.append(relation) 
 
             subtitle_interval.append(line)
         else:
@@ -54,9 +60,9 @@ def keyword_search(name_file, relationship_file, subtitle_file):
                     time_to_keyword.pop()
             subtitle_interval = []
             keyword_number = 0
-    
-    csv_io.write_csv(OUTPUT_ROOT_PATH + 'keywordSearch.csv',time_to_keyword)
 
+    csv_io.write_csv(OUTPUT_ROOT_PATH + 'keywordSearch.csv', time_to_keyword)
+    csv_io.write_csv(OUTPUT_ROOT_PATH + 'keywordList.csv', [keyword_list])
 
 def read_subtitle_file(subtitle_file):
 
