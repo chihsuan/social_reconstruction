1ad8638cdcc78da926be5723cc8d16f33e498449
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-07-31 15:02:30 +0800

    add new modules

--- module/csv_io.py
+++ module/csv_io.py
@@ -3,9 +3,10 @@ import csv
 def read_csv(file_name):
     data = [] 
     with open(file_name, 'r') as input_file:
-        data = csv.reader(input_file).next()
-
-    return data
+        reader = csv.reader(input_file)
+        for row in reader:
+            data.append(row)
+    return data[0] if len(data) == 1 else data
 
 def write_csv(file_name, content):
     with open(file_name, 'w') as output_file:
