49ee5c55a8682bac52538cd3bb6b230a4f0355e7
chi-hsuan <chihsuan.huang.7@gmail.com>
2014-08-04 15:19:06 +0800

    change find entitiy set method

--- find_entity_set.py
+++ find_entity_set.py
@@ -17,41 +17,81 @@ from modules import json_io
 from modules import time_format
 
 OUTPUT_PAHT = 'output/'
-MIN_INTETVAL = 3
 
-def find_two_entity(keyword_list_file, search_result_file):
+def find_two_entity(keyword_list_file, search_result_file, relations_file):
 
     time_to_keyword = csv_io.read_csv(search_result_file)
     keyword_list = csv_io.read_csv(keyword_list_file)
+    relation_list = json_io.read_json(relations_file)
+
     leading_keyword = keyword_list[0]
     
     frame_to_keyword = {}
     for row in time_to_keyword:
         start_frame, end_frame = time_format.to_frame(row)
+        while start_frame in frame_to_keyword:
+            start_frame = start_frame + 0.001
+        while end_frame in frame_to_keyword:
+            end_frame = end_frame + 0.001
         frame_to_keyword[start_frame] = row[1]
     
     # Transfrom to timeline format
     frame_list = frame_to_keyword.keys()
     frame_list.sort()
     
+    entity_set = {}
+
     # Find two entity with leadingKeword
-    leading_entity_set, relation_set = get_adjacent(leading_keyword, frame_list, frame_to_keyword, 24 * 60 * MIN_INTETVAL)
-
+    for name, relations in relation_list.iteritems():
+        min_interval = 5
+        if name != leading_keyword:
+            role_set = {}
+            important_person = max(relation_list[name], key=relation_list[name].get)
+            key = name + '-' + important_person
+            while min_interval >= 0.5 and key not in role_set: 
+                role_set, relation_set = get_adjacent(name, frame_list, frame_to_keyword, 24 * 60 * min_interval)
+                min_interval -= 0.5
+            if key in role_set:
+                entity_set.update({key : role_set[key]})
+            else:
+                important_person = max(role_set, key=role_set.get)
+                entity_set.update({important_person: role_set[important_person]})
     # Add leading_keyword list
-    leading_entity_set.update(leading_keword_filter(leading_keyword, frame_list, frame_to_keyword, 24 * 60 * 1 ))
+    entity_set.update(leading_keword_filter(leading_keyword, frame_list, frame_to_keyword, 24 * 60 * 1 ))
+    relation_set.append(leading_keyword)
+  
+    '''add_role = []
+    # Add other important roles
+    for name, relations in relation_list.iteritems():
+        if name in relation_set:
+            for relation in relations:
+                if relation not in relation_set and relation not in add_role:
+                    add_role.append(relation)
 
-    # Add keywords that not found in last step 
-    keyword_frames = {}
+    for role in add_role:
+        important_person = max(relation_list[role], key=relation_list[role].get)
+        role_set, rel_set = get_adjacent(important_person, frame_list, frame_to_keyword, 24 * 60 * (MIN_INTETVAL-1) )
+        entity_set.update(role_set)
+    
+    # Add supporting role who are not imporant in movie
+    for keyword in keyword_list:
+        if keyword not in relation_set and keyword not in add_role:
+            role_set, rel_set = get_adjacent(keyword, frame_list, frame_to_keyword, 24 * 60)
+            important_person = max(role_set, key=role_set.get)
+            entity_set.update({important_person : role_set[important_person]})
+   
+    print entity_set'''
+
+    '''keyword_frames = {}
     for frame in frame_to_keyword:
         keyword = frame_to_keyword[frame]
         if frame_to_keyword[frame] not in relation_set and frame_to_keyword[frame] != leading_keyword:
             if keyword not in keyword_frames:
                 keyword_frames[keyword] = []
-            keyword_frames[keyword].append(frame)
+            keyword_frames[keyword].append(frame)'''
 
-    leading_entity_set.update(keyword_frames)
 
-    json_io.write_json( OUTPUT_PAHT + 'entity_set.json',leading_entity_set)
+    json_io.write_json( OUTPUT_PAHT + 'entity_set.json', entity_set)
 
 
 def get_adjacent(pivot_keyword, frame_list, frame_to_keyword, interval):
@@ -123,6 +163,6 @@ def leading_keword_filter(leading_keyword, frame_list, frame_to_keyword, interva
 
 if __name__=='__main__':
     if len(sys.argv) == 3:
-        find_two_entity(sys.argv[1], sys.argv[2])
+        find_two_entity(sys.argv[1], sys.argv[2], sys.argv[3])
     else:
-        reconstruct_role('output/keyword_list.csv', 'output/search_result.csv')
+        find_two_entity('output/keyword_list.csv', 'output/search_result.csv', 'output/relations.json')
